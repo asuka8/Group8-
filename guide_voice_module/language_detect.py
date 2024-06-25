43ed26feffdae4781ceb703801bae9a40671bd21
Zengqing Wu <75557839+wuzengqing001225@users.noreply.github.com>
2024-06-25 16:50:01 +0900

    Add files via upload

---
+++ guide_voice_module/language_detect.py
@@ -0,0 +1,15 @@
+from langdetect import detect
+
+def detect_language(text):
+    try:
+        lang = detect(text)
+        if lang == 'en':
+            return "en"
+        elif lang == 'ja':
+            return "ja"
+        elif lang == 'zh-cn' or lang == 'zh-tw':
+            return "zn"
+        else:
+            return "Unknown language"
+    except Exception as e:
+        return str(e)
