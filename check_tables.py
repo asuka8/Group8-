8a2851b22e59580ccbb421e7440da5927792039e
oouji <kyousukesouhei@gmail.com>
2024-07-09 14:28:10 +0900

    login関連を繋げた

--- check_tables.py
+++ check_tables.py
@@ -1,9 +1,7 @@
 from app import app, db
 
-from app import app, db
-
 with app.app_context():
-    with db.get_engine().connect() as connection:
+    with db.engine.connect() as connection:
         result = connection.execute("SELECT name FROM sqlite_master WHERE type='table';")
         for row in result:
             print(row[0])
